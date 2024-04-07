453e4faf4685f6270ebe3d4fb89a2126cae716ff
zaidNsour <nsourzaid1@gmail.com>
2024-04-07 11:52:50 +0300

    delete if__name__ =

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from website import app
 
-if __name__ == "__main__": #run the web server if we directly run this file
-  app.run(port=5000, host='0.0.0.0')
+'''if __name__ == "__main__": #run the web server if we directly run this file'''
+app.run(port=5000, host='0.0.0.0')
 
