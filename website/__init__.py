e315e13d8996f13d19028d10d73a53495a8ed8cd
zaidNsour <nsourzaid1@gmail.com>
2024-04-30 15:13:01 +0300

    fix some issue

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_ckeditor import CKEditor
-from flask_mail import Mail
+#from flask_mail import Mail
 
 
 def create_database(app):
@@ -27,7 +27,7 @@ app.config['MAIL_PORT']= 587
 app.config['MAIL_USE_TLS']= True
 #app.config['MAIL_USERNAME']= os.environ.get('EMAIL_USER')
 #app.config['MAIL_PASSWORD']= os.environ.get('EMAIL_PASS')
-mail=Mail(app)
+#mail=Mail(app)
 
 app.app_context().push()
 
